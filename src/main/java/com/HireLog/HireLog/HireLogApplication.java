f8867500703bd54a569ff07cdd7035bdd71556d9
Sumeet1016 <yadavsumeet566@gmail.com>
2026-01-14 21:36:50 +0530

    Initial Spring Boot Setup

---
+++ src/main/java/com/HireLog/HireLog/HireLogApplication.java
@@ -0,0 +1,13 @@
+package com.HireLog.HireLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HireLog/HireLog/HireLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HireLog.HireLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
