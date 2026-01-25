0351236659ebd99f0f53df7799a4bb1495df65ad
Sumeet1016 <yadavsumeet566@gmail.com>
2026-01-25 19:04:03 +0530

    feat: Add Header  and Footer

---
+++ hirelog-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+function App() {
+
+  return <div>
+    <Navbar/>
+    <Footer></Footer>
+    </div>
+}
+
+export default App
---
+++ hirelog-frontend/src/Components/Navbar.jsx
@@ -0,0 +1,68 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid custom-navbar">
+        {/* LEFT */}
+        <a className="navbar-brand nav-left" href="#">
+          HireLog
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse nav-center"
+          id="navbarNavDropdown"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Guest-User
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        {/* RIGHT */}
+        <div className="nav-right">
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                data-bs-toggle="dropdown"
+              >
+                Add Job
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
